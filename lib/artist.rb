f0683af279f60deecebed45f16c4d0809702b18b
ode-to-the-code <justcallmethis@gmail.com>
2018-03-27 17:36:45 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,76 +1,132 @@
-require 'pry'
+# require 'pry'
+#
+# class Artist
+# attr_accessor :name
+# @@all = []
+#
+#
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#     #@@all << self
+#   end
+#
+#   # def self.find_or_create_by_name(artists_name_here)
+#   #
+#   # @@all.each do |item|
+#   #   if item.name == artists_name_here
+#   #     item
+#   #   elsif !item.name == artists_name_here
+#   #     new_artist = Artist.new(artists_name_here)
+#   #     @@all << new_artist
+#   #     new_artist
+#   #     binding.pry
+#   #   elsif !item.name == artists_name_here
+#   #     new_artist = Artist.new(artists_name_here)
+#   #     @@all << new_artist
+#   #     new_artist
+#   #   end
+#   # end
+#   #
+#   # end
+#
+#   def self.find_or_create_by_name(artists_name_here)
+#     this_artist = @@all.detect{|artist| artist.name == artists_name_here}
+#     if this_artist
+#       this_artist
+#     else
+#       this_artist = Artist.new(artists_name_here)
+#       this_artist.save
+#
+#   # @@all.each do |item|
+#   #   if item.name == artists_name_here
+#   #     return item
+#   #   else
+#   #     new_artist = Artist.new(artists_name_here)
+#   #     return new_artist
+#   #   end
+#   end
+#
+#   end
+#
+#   def add_song(song)
+#     @songs << song
+#   end
+#
+#   def songs
+#     @songs
+#   end
+#
+#   def save
+#     @@all << self
+#     self
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+#
+#   def print_songs
+#     @songs.each do |song|
+#     puts song.name
+#     end
+#   end
+#
+#
+# end
+
 
 class Artist
-attr_accessor :name
-@@all = []
-
+  attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
-    #@@all << self
-  end
-
-  # def self.find_or_create_by_name(artists_name_here)
-  #
-  # @@all.each do |item|
-  #   if item.name == artists_name_here
-  #     item
-  #   elsif !item.name == artists_name_here
-  #     new_artist = Artist.new(artists_name_here)
-  #     @@all << new_artist
-  #     new_artist
-  #     binding.pry
-  #   elsif !item.name == artists_name_here
-  #     new_artist = Artist.new(artists_name_here)
-  #     @@all << new_artist
-  #     new_artist
-  #   end
-  # end
-  #
-  # end
-
-  def self.find_or_create_by_name(artists_name_here)
-    this_artist = @@all.detect{|artist| artist.name == artists_name_here}
-    if this_artist
-      this_artist
-    else
-      this_artist = Artist.new(artists_name_here)
-      this_artist.save
-
-  # @@all.each do |item|
-  #   if item.name == artists_name_here
-  #     return item
-  #   else
-  #     new_artist = Artist.new(artists_name_here)
-  #     return new_artist
-  #   end
-  end
-
-  end
-
-  def add_song(song)
-    @songs << song
   end
 
   def songs
     @songs
   end
 
-  def save
-    @@all << self
-    self
+  def add_song(song)
+    @songs << song
   end
 
   def self.all
     @@all
   end
 
-  def print_songs
-    @songs.each do |song|
-    puts song.name
+  def save
+    @@all << self
+  end
+
+
+  def self.find_or_create_by_name(name)
+    # binding.pry
+    if Artist.all.each do |artist|
+      artist.name == name
+      return artist
+      # binding.pry
+    end
+      # binding.pry
+      # artist1 = self.new(name)
+      # binding.pry
+      # @@all = @@all.uniq
+      # binding.pry
+    else
+      x = Artist.new(name)
+      # binding.pry
     end
   end
 
+
+  def print_songs
+    self.songs.each do |song|
+      # binding.pry
+      puts "#{song}"
+      binding.pry
+    end
+  end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,36 @@
+# class Song
+#   attr_accessor :name, :artist
+#
+#   def initialize(name)
+#     @name = name
+#   end
+#
+#   def artist_name=(name)
+#     Artist.new(name)
+#   end
+#
+#   def self.new_by_filename(file_name)
+#     file_name = file_name.split(" - ")
+#   # use not slice, bu tthe method i used before to take off '.mp3' four palces from back
+#     song_name = file_name[1]
+#     new_song_instance = Song.new(song_name)
+#     # new_song_instance = artist_name=(file_name[0])
+#     x = Artist.find_or_create_by_name(file_name[0])
+#     new_song_instance.artist = x
+#     x.songs << new_song_instance
+#     new_song_instance
+#   end
+#
+#
+#
+# end
+
+
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :title
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
   end
-
-  def artist_name=(name)
-    Artist.new(name)
-  end
-
-  def self.new_by_filename(file_name)
-    file_name = file_name.split(" - ")
-  # use not slice, bu tthe method i used before to take off '.mp3' four palces from back
-    song_name = file_name[1]
-    new_song_instance = Song.new(song_name)
-    # new_song_instance = artist_name=(file_name[0])
-    x = Artist.find_or_create_by_name(file_name[0])
-    new_song_instance.artist = x
-    x.songs << new_song_instance
-    new_song_instance
-  end
-
-
 
 end
